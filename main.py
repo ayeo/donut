4e48865738215e047890458b6da620e4b2ba04a7
ayeo <me@ayeo.pl>
2021-03-05 20:22:19 +0100

    Another commit to keep my CAN streak going.

--- main.py
+++ main.py
@@ -9,8 +9,33 @@ smallRadius = 300
 pygame.init()
 screen = pygame.display.set_mode([800, 800])
 
+structure = np.empty((3,))
+
+for alpha in range(0, 360, 10):
+    radians = math.radians(alpha)
+    x = round(radius * math.cos(radians))
+    y = round(radius * math.sin(radians))
+    z = 0
+
+    for psi in range(0, 360, 20):
+        radians = math.radians(psi)
+        xs = round(smallRadius * math.sin(radians))
+        ys = 0
+        zs = round(smallRadius * math.cos(radians))
+
+        angle = math.radians(alpha)
+        rotateZZ = np.array([[math.cos(angle), -1 * math.sin(angle), 0], [math.sin(angle), math.cos(angle), 0], [0, 0, 1]])
+        position = [xs, ys, zs]
+        position = rotateZZ.dot(position)
+        position = position + np.array([x, y, z])
+        structure = np.vstack((structure,position))
+
 while True:
     for rot in range(0, 360, 1):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit(0)
+
         screen.fill((0, 0, 0))
 
         angle = math.radians(rot)
@@ -18,51 +43,28 @@ while True:
         rotateY = np.array([[math.cos(angle), 0, math.sin(angle)], [0, 1, 0], [-1 * math.sin(angle), 0, math.cos(angle)]])
         rotateZ = np.array([[math.cos(angle), -1 * math.sin(angle), 0], [math.sin(angle), math.cos(angle), 0], [0, 0, 1]])
 
-        for alpha in range(0, 360, 20):
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    quit(0)
+        for position in structure:
+            position = rotateY.dot(position)
+            position = rotateX.dot(position)
+            x1 = position[0]
+            y1 = position[1]
+            z1 = position[2] + 500
 
-            radians = math.radians(alpha)
-            x = round(radius * math.cos(radians))
-            y = round(radius * math.sin(radians))
-            z = 0
+            xx = 0.3 * z1 * x1/z1 + 400
+            yy = 0.3 * z1 * y1/z1 + 400
 
-            for psi in range(0, 360, 60):
-                radians = math.radians(psi)
-                xs = round(smallRadius * math.sin(radians))
-                ys = 0
-                zs = round(smallRadius * math.cos(radians))
+            cc = 150 + z1/2 - 250
+            # print(cc)
+            if cc > 255:
+                cc = 255
+            if cc < 30:
+                cc = 30
 
-                angle = math.radians(alpha)
-                rotateZZ = np.array([[math.cos(angle), -1 * math.sin(angle), 0], [math.sin(angle), math.cos(angle), 0], [0, 0, 1]])
-                position = [xs, ys, zs]
-                position = rotateZZ.dot(position)
+            color = (cc, cc, cc)
+            pygame.draw.circle(screen, color, (xx, yy), 2)
 
-                position = position + np.array([x, y, z])
-
-                position = rotateY.dot(position)
-                position = rotateX.dot(position)
-                x1 = position[0]
-                y1 = position[1]
-                z1 = position[2] + 500
-
-                xx = 0.3 * z1 * x1/z1 + 400
-                yy = 0.3 * z1 * y1/z1 + 400
-
-                cc = 150 + z1/2 - 250
-                # print(cc)
-                if cc > 255:
-                    cc = 255
-                if cc < 30:
-                    cc = 30
-
-                color = (cc, cc, cc)
-                pygame.draw.circle(screen, color, (xx, yy), 2)
-
-
-        pygame.display.flip()
         time.sleep(0.01)
+        pygame.display.flip()
 
 pygame.quit()
 
