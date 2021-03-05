8ff56b06091e187bffb12c48c5a48aecf38827e8
ayeo <me@ayeo.pl>
2021-03-05 20:00:47 +0100

    Fixed unnecessary bug.

--- main.py
+++ main.py
@@ -18,9 +18,7 @@ while True:
         rotateY = np.array([[math.cos(angle), 0, math.sin(angle)], [0, 1, 0], [-1 * math.sin(angle), 0, math.cos(angle)]])
         rotateZ = np.array([[math.cos(angle), -1 * math.sin(angle), 0], [math.sin(angle), math.cos(angle), 0], [0, 0, 1]])
 
-
-
-        for alpha in range(0, 360, 5):
+        for alpha in range(0, 360, 20):
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     quit(0)
@@ -30,7 +28,7 @@ while True:
             y = round(radius * math.sin(radians))
             z = 0
 
-            for psi in range(0, 360, 20):
+            for psi in range(0, 360, 60):
                 radians = math.radians(psi)
                 xs = round(smallRadius * math.sin(radians))
                 ys = 0
@@ -53,7 +51,7 @@ while True:
                 yy = 0.3 * z1 * y1/z1 + 400
 
                 cc = 150 + z1/2 - 250
-                print(cc)
+                # print(cc)
                 if cc > 255:
                     cc = 255
                 if cc < 30:
@@ -64,7 +62,7 @@ while True:
 
 
         pygame.display.flip()
-        # time.sleep(0.01)
+        time.sleep(0.01)
 
 pygame.quit()
 
