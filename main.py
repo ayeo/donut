eb83d55ac85636ffe41abd728651b97319ae7dcc
ayeo <me@ayeo.pl>
2021-03-05 21:13:02 +0100

    tagging release w.t.f.

--- main.py
+++ main.py
@@ -9,15 +9,19 @@ smallRadius = 300
 pygame.init()
 screen = pygame.display.set_mode([800, 800])
 
-structure = np.empty((3,))
+# structure = np.empty((0,3))
 
+
+large_circles = np.empty((0,18,3))
 for alpha in range(0, 360, 10):
     radians = math.radians(alpha)
     x = round(radius * math.cos(radians))
     y = round(radius * math.sin(radians))
     z = 0
 
+    small_circles = np.empty((0,3))
     for psi in range(0, 360, 20):
+
         radians = math.radians(psi)
         xs = round(smallRadius * math.sin(radians))
         ys = 0
@@ -28,7 +32,9 @@ for alpha in range(0, 360, 10):
         position = [xs, ys, zs]
         position = rotateZZ.dot(position)
         position = position + np.array([x, y, z])
-        structure = np.vstack((structure,position))
+        small_circles = np.vstack((small_circles, position))
+
+    large_circles = np.vstack((large_circles, [small_circles]))
 
 while True:
     for rot in range(0, 360, 1):
@@ -43,25 +49,26 @@ while True:
         rotateY = np.array([[math.cos(angle), 0, math.sin(angle)], [0, 1, 0], [-1 * math.sin(angle), 0, math.cos(angle)]])
         rotateZ = np.array([[math.cos(angle), -1 * math.sin(angle), 0], [math.sin(angle), math.cos(angle), 0], [0, 0, 1]])
 
-        for position in structure:
-            position = rotateY.dot(position)
-            position = rotateX.dot(position)
-            x1 = position[0]
-            y1 = position[1]
-            z1 = position[2] + 500
+        for structure in large_circles:
+            for position in structure:
+                position = rotateY.dot(position)
+                position = rotateX.dot(position)
+                x1 = position[0]
+                y1 = position[1]
+                z1 = position[2] + 500
 
-            xx = 0.3 * z1 * x1/z1 + 400
-            yy = 0.3 * z1 * y1/z1 + 400
+                xx = 0.3 * z1 * x1/z1 + 400
+                yy = 0.3 * z1 * y1/z1 + 400
 
-            cc = 150 + z1/2 - 250
-            # print(cc)
-            if cc > 255:
-                cc = 255
-            if cc < 30:
-                cc = 30
+                cc = 150 + z1/2 - 250
+                # print(cc)
+                if cc > 255:
+                    cc = 255
+                if cc < 30:
+                    cc = 30
 
-            color = (cc, cc, cc)
-            pygame.draw.circle(screen, color, (xx, yy), 2)
+                color = (cc, cc, cc)
+                pygame.draw.circle(screen, color, (xx, yy), 2)
 
         time.sleep(0.01)
         pygame.display.flip()
