e495a7ed77a3f3b9d033f1dd5c765eb344fdb27a
ayeo <me@ayeo.pl>
2021-03-05 16:01:29 +0100

    That last commit message about silly mistakes pales in comparision to this one

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import math
 import time
 import pygame
 
-radius = 400
+radius = 1000
+smallRadius = 300
+
 pygame.init()
 screen = pygame.display.set_mode([800, 800])
 
@@ -16,7 +18,9 @@ while True:
         rotateY = np.array([[math.cos(angle), 0, math.sin(angle)], [0, 1, 0], [-1 * math.sin(angle), 0, math.cos(angle)]])
         rotateZ = np.array([[math.cos(angle), -1 * math.sin(angle), 0], [math.sin(angle), math.cos(angle), 0], [0, 0, 1]])
 
-        for alpha in range(0, 360, 10):
+
+
+        for alpha in range(0, 360, 5):
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     quit(0)
@@ -26,26 +30,41 @@ while True:
             y = round(radius * math.sin(radians))
             z = 0
 
-            position = rotateY.dot([x, y, z])
-            position = rotateX.dot(position)
-            x = position[0]
-            y = position[1]
-            z = position[2] + 500
+            for psi in range(0, 360, 20):
+                radians = math.radians(psi)
+                xs = round(smallRadius * math.sin(radians))
+                ys = 0
+                zs = round(smallRadius * math.cos(radians))
 
-            xx = 0.3 * z * x/z + 400
-            yy = 0.3 * z * y/z + 400
+                angle = math.radians(alpha)
+                rotateZZ = np.array([[math.cos(angle), -1 * math.sin(angle), 0], [math.sin(angle), math.cos(angle), 0], [0, 0, 1]])
+                position = [xs, ys, zs]
+                position = rotateZZ.dot(position)
 
-            cc = 150 + z/2 - 250
-            if cc > 255:
-                cc = 255
-            if cc < 30:
-                cc = 30
+                position = position + np.array([x, y, z])
 
-            color = (cc, cc, cc)
-            pygame.draw.circle(screen, color, (xx, yy), 2)
+                position = rotateY.dot(position)
+                position = rotateX.dot(position)
+                x1 = position[0]
+                y1 = position[1]
+                z1 = position[2] + 500
+
+                xx = 0.3 * z1 * x1/z1 + 400
+                yy = 0.3 * z1 * y1/z1 + 400
+
+                cc = 150 + z1/2 - 250
+                print(cc)
+                if cc > 255:
+                    cc = 255
+                if cc < 30:
+                    cc = 30
+
+                color = (cc, cc, cc)
+                pygame.draw.circle(screen, color, (xx, yy), 2)
+
 
         pygame.display.flip()
-        time.sleep(0.01)
+        # time.sleep(0.01)
 
 pygame.quit()
 
